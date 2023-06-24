56e7eae97f3a0126cb7b0c27b7b7d13cfd5de46f
andreslopezlu <andreslopezlu@gmail.com>
2023-06-23 23:38:13 -0500

    project orm version - wip - pending: FrontEnd (there is progress in log/login and admin profile views (delete, show all, modify and save full methods)), logging, test, security, FrontEnd validations, handling exceptions

--- target/classes/static/js_home/admin_pacientes.js
+++ target/classes/static/js_home/admin_pacientes.js
@@ -1,19 +1,18 @@
 
 window.addEventListener("load", function(){
 
-
+// ----------------------------------------------------------------------------------------------------------------------- //
     
     let table = this.document.querySelector("table");
     let adminPacientes = document.querySelector(".admin-pacientes");
-    let agregar = document.querySelector(".agregar");
-    let formularioModificar = document.querySelector("form");
+    let formularioModificar = document.querySelector("form.formulario-modificar");
+    let formularioAgregar = document.querySelector("form.formulario-agregar");
 
     table.style.display = "none";
     formularioModificar.style.display = "none";
+    formularioAgregar.style.display = "none";
 
-
-
-    let endpointGetAllPacientes = "/final/pacientes/get/all";
+// ----------------------------------------------------------------------------------------------------------------------- //
 
     function renderizarPacientes(data){
     
@@ -52,7 +51,25 @@ window.addEventListener("load", function(){
         }
     }
 
+    function renderizarAgregar(){
+
+        let agregarNuevo = document.querySelector("div.agregar");
+        let buttonAgregarNuevo = document.querySelector("button.agregar-nuevo");
+
+        if(buttonAgregarNuevo){
+            buttonAgregarNuevo.remove();
+        }
+
+        buttonAgregarNuevo = document.createElement("button");
+        buttonAgregarNuevo.innerText = "Agregar Paciente";
+        buttonAgregarNuevo.classList.add("agregar-nuevo");
+        agregarNuevo.appendChild(buttonAgregarNuevo);
+
+    }
+
     function consultarPacientes(){
+
+            let endpointGetAllPacientes = "/final/pacientes/get/all";
 
         const settings = {
             method: 'GET',
@@ -68,13 +85,17 @@ window.addEventListener("load", function(){
             .then((data) => {
                 console.log(data);
                 renderizarPacientes(data);
+                renderizarAgregar();
                 eliminarPaciente();
-                modificarPaciente();
+                consultarPaciente();
+                agregarPaciente();
             })
             .catch((error) => {
                 console.error(error);
             })
     }
+
+// ----------------------------------------------------------------------------------------------------------------------- //
 
     function eliminarPaciente(){
     
@@ -106,16 +127,13 @@ window.addEventListener("load", function(){
         )
     }
 
-
-
-
-
+// ----------------------------------------------------------------------------------------------------------------------- //
 
     function renderizarPaciente(data){
 
-        let formularioModificar = document.querySelector("form");
+        let formularioModificar = document.querySelector("form.formulario-modificar");
 
-        let dataFormulario = document.querySelector("form div");
+        let dataFormulario = document.querySelector("form.formulario-modificar div");
 
         if(dataFormulario){
             dataFormulario.remove();
@@ -135,58 +153,65 @@ window.addEventListener("load", function(){
         </div>
         <div>
             <label for="nombre">Nombre</label>
-            <input type="text" name="nombre" id="nombre" autocomplete="off" value=${data.nombre}>
+            <input type="text" name="nombre" id="nombre" autocomplete="off" onsubmit="required()" placeholder="Pepito" value=${data.nombre}>
         </div>
         <div>
             <label for="apellido">Apellido</label>
-            <input type="text" name="apellido" id="apellido" autocomplete="off" value=${data.apellido}>
+            <input type="text" name="apellido" id="apellido" autocomplete="off" onsubmit="required()" placeholder="Perez" value=${data.apellido}>
         </div>
         <div>
             <label for="dni">DNI</label>
-            <input type="text" name="dni" id="dni" autocomplete="off" value=${data.dni}>
+            <input type="text" name="dni" id="dni" autocomplete="off" onsubmit="required()" placeholder="1234567890" value=${data.dni}>
         </div>
         <p>Domicilio</p>
         <div>
             <label for="domicilio">Calle</label>
-            <input type="text" name="domicilio" id="domicilio" autocomplete="off" value=${data.domicilio.calle}>
+            <input type="text" name="calle" id="calle" autocomplete="off" onsubmit="required()" placeholder="Robles" value=${data.domicilio.calle}>
         </div>
         <div>
             <label for="domicilio">Numero</label>
-            <input type="text" name="domicilio" id="domicilio" autocomplete="off" value=${data.domicilio.numero}>
+            <input type="text" name="numero" id="numero" autocomplete="off" onsubmit="required()" placeholder="48" value=${data.domicilio.numero}>
         </div>
         <div>
             <label for="domicilio">Provincia</label>
-            <input type="text" name="domicilio" id="domicilio" autocomplete="off" value=${data.domicilio.provincia}>
+            <input type="text" name="provincia" id="provincia" autocomplete="off" onsubmit="required()" placeholder="Cund" value=${data.domicilio.provincia}>
         </div>
         <div>
             <label for="domicilio">País</label>
-            <input type="text" name="domicilio" id="domicilio" autocomplete="off" value=${data.domicilio.pais}>
+            <input type="text" name="pais" id="pais" autocomplete="off" onsubmit="required()" placeholder="Col" value=${data.domicilio.pais}>
         </div>
         <div>
             <label for="domicilio">Codigo postal</label>
-            <input type="text" name="domicilio" id="domicilio" autocomplete="off" value=${data.domicilio.codigoPostal}>
+            <input type="text" name="codigoPostal" id="codigoPostal" autocomplete="off" onsubmit="required()" placeholder="110141" value=${data.domicilio.codigoPostal}>
         </div>
         <p>Otros datos</p>
         <div>
             <label for="fecha-alta">Fecha alta</label>
-            <input type="text" name="fecha-alta" id="fecha-alta" autocomplete="off" value=${data.fechaAlta}>
+            <input type="text" name="fechaAlta" id="fechaAlta" autocomplete="off" onsubmit="required()" placeholder="2023-03-10"value=${data.fechaAlta}>
         </div>
         </section>
         <br>
-        <section class="submission">
-        <input class="login" type="submit" name="submit" id="submit" value="Aceptar modificación">
+        <section class="accept-modifications">
+        <input class="accept-modifications" type="submit" value="Aceptar modificación">
         </section>`
 
         formularioModificar.appendChild(dataFormulario);
 
     }
 
-    function modificarPaciente(){
+    function consultarPaciente(){
 
         let botonesModificar = document.querySelectorAll("button.modificar");
 
         botonesModificar.forEach(boton => {
             boton.addEventListener("click", (e) => {
+
+                let formularioAgregar = document.querySelector("form.formulario-agregar");
+
+                if(formularioAgregar){
+                    formularioAgregar.style.display = "none";
+                }
+
                 let id = boton.getAttribute("id");
 
                 let endpointGetPaciente = `/final/pacientes/get/${id}`;
@@ -202,13 +227,239 @@ window.addEventListener("load", function(){
                     .then((data) => {
                         console.log(data);
                         renderizarPaciente(data);
+                        aceptarModificacionPaciente();
                     })
                     .catch((error) => {
                         console.error(error);
                     })  
             })
         })
+    }   
+
+    function capturarModificacionPaciente(){
+
+        let idPaciente = document.querySelector("input#id").value;
+        let nombrePaciente = document.querySelector("input#nombre").value;
+        let apellidoPaciente = document.querySelector("input#apellido").value;
+        let dniPaciente = document.querySelector("input#dni").value;
+
+        let calleDomicilio = document.querySelector("input#calle").value;
+        let numeroDomicilio = document.querySelector("input#numero").value;
+        let provinciaDomicilio = document.querySelector("input#provincia").value;
+        let paisDomicilio = document.querySelector("input#pais").value;
+        let codigoPostalDomicilio = document.querySelector("input#codigoPostal").value;
+
+        let fechaAltaPaciente = document.querySelector("input#fechaAlta").value;
+
+        let paciente = {
+            id: idPaciente,
+            nombre: nombrePaciente,
+            apellido: apellidoPaciente,
+            dni: dniPaciente,
+            domicilio: {
+                calle: calleDomicilio,
+                numero: numeroDomicilio,
+                provincia: provinciaDomicilio,
+                pais: paisDomicilio,
+                codigoPostal: codigoPostalDomicilio,
+            },
+            fechaAlta: fechaAltaPaciente
+        }
+
+        return paciente;
+
     }
+
+    function aceptarModificacionPaciente(){
+
+        let aceptarModificacion = document.querySelector(".accept-modifications");
+
+        aceptarModificacion.addEventListener("click", (e) => {
+
+            e.preventDefault();
+
+            let paciente = capturarModificacionPaciente();
+
+            let endpointPutPaciente = "/final/pacientes/put";
+
+            const settings = {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+                body: JSON.stringify(paciente)
+            }
+
+            fetch(endpointPutPaciente, settings)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((data) => {
+                        console.log(data);
+                        consultarPacientes();
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    })  
+        })
+    }
+
+// ----------------------------------------------------------------------------------------------------------------------- //
+
+function renderizarFormularioNuevoPaciente(){
+
+    let formularioAgregar = document.querySelector("form.formulario-agregar");
+
+    let dataFormulario = document.querySelector("form.formulario-agregar div");
+
+    if(dataFormulario){
+        dataFormulario.remove();
+    }
+
+    formularioAgregar.style.display = "initial";
+
+    dataFormulario = document.createElement("div");
+    dataFormulario.setAttribute("class", "data");
+
+    dataFormulario.innerHTML = 
+    `<section>
+    <p>Datos basicos</p>
+    <div>
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" id="nombre" autocomplete="off" onsubmit="required()" placeholder="Pepito">
+    </div>
+    <div>
+        <label for="apellido">Apellido</label>
+        <input type="text" name="apellido" id="apellido" autocomplete="off" onsubmit="required()" placeholder="Perez">
+    </div>
+    <div>
+        <label for="dni">DNI</label>
+        <input type="text" name="dni" id="dni" autocomplete="off" onsubmit="required()" placeholder="1234567890">
+    </div>
+    <p>Domicilio</p>
+    <div>
+        <label for="domicilio">Calle</label>
+        <input type="text" name="calle" id="calle" autocomplete="off" onsubmit="required()" placeholder="Robles">
+    </div>
+    <div>
+        <label for="domicilio">Numero</label>
+        <input type="text" name="numero" id="numero" autocomplete="off" onsubmit="required()" placeholder="48">
+    </div>
+    <div>
+        <label for="domicilio">Provincia</label>
+        <input type="text" name="provincia" id="provincia" autocomplete="off" onsubmit="required()" placeholder="Cund">
+    </div>
+    <div>
+        <label for="domicilio">País</label>
+        <input type="text" name="pais" id="pais" autocomplete="off" onsubmit="required()" placeholder="Col">
+    </div>
+    <div>
+        <label for="domicilio">Codigo postal</label>
+        <input type="text" name="codigoPostal" id="codigoPostal" autocomplete="off" onsubmit="required()" placeholder="110141">
+    </div>
+    <p>Otros datos</p>
+    <div>
+        <label for="fecha-alta">Fecha alta</label>
+        <input type="text" name="fechaAlta" id="fechaAlta" autocomplete="off" onsubmit="required()" placeholder="2023-03-10">
+    </div>
+    </section>
+    <br>
+    <section class="agregar-nuevo-paciente">
+    <input class="button-agregar-nuevo-paciente" type="submit" value="Guardar paciente">
+    </section>
+    <br>`
+
+    formularioAgregar.appendChild(dataFormulario);
+
+}
+
+function capturarNuevoPaciente(){
+
+    let nombrePaciente = document.querySelector("input#nombre").value;
+    let apellidoPaciente = document.querySelector("input#apellido").value;
+    let dniPaciente = document.querySelector("input#dni").value;
+
+    let calleDomicilio = document.querySelector("input#calle").value;
+    let numeroDomicilio = document.querySelector("input#numero").value;
+    let provinciaDomicilio = document.querySelector("input#provincia").value;
+    let paisDomicilio = document.querySelector("input#pais").value;
+    let codigoPostalDomicilio = document.querySelector("input#codigoPostal").value;
+
+    let fechaAltaPaciente = document.querySelector("input#fechaAlta").value;
+
+    let paciente = {
+        nombre: nombrePaciente,
+        apellido: apellidoPaciente,
+        dni: dniPaciente,
+        domicilio: {
+            calle: calleDomicilio,
+            numero: numeroDomicilio,
+            provincia: provinciaDomicilio,
+            pais: paisDomicilio,
+            codigoPostal: codigoPostalDomicilio,
+        },
+        fechaAlta: fechaAltaPaciente
+    }
+
+    return paciente;
+
+}
+
+function agregarPaciente(){
+
+    let nuevoPaciente = document.querySelector("button.agregar-nuevo");
+    
+    nuevoPaciente.addEventListener("click", (e) => {
+
+        let formularioModificar = document.querySelector("form.formulario-modificar");
+
+            if(formularioModificar){
+                formularioModificar.style.display = "none";
+            }
+
+        console.log("aqui estoy")
+        
+        renderizarFormularioNuevoPaciente();
+        aceptarGuardarPaciente();
+    })
+
+}
+
+function aceptarGuardarPaciente(){
+
+    let nuevoPaciente = document.querySelector("input.button-agregar-nuevo-paciente");
+
+    nuevoPaciente.addEventListener("click", (e) => {
+
+        e.preventDefault();
+
+        let paciente = capturarNuevoPaciente();
+
+        let endpointPutPaciente = "/final/pacientes/post";
+
+        const settings = {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+            body: JSON.stringify(paciente)
+        }
+
+        fetch(endpointPutPaciente, settings)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    consultarPacientes();
+                })
+                .catch((error) => {
+                    console.error(error);
+                })  
+    })
+}
+
+// ----------------------------------------------------------------------------------------------------------------------- //
 
     adminPacientes.addEventListener("click", (e) => {
         table.style.display = "initial";
@@ -216,11 +467,13 @@ window.addEventListener("load", function(){
         let adminTitle = document.querySelector(".admin");
         adminTitle.innerText = "Pacientes";
 
-        agregar.innerText = "Agregar paciente";
+        // agregar.innerText = "Agregar paciente";
+
+        // agregar.style.display = "initial";
 
         consultarPacientes();
     })
 
-
+// ----------------------------------------------------------------------------------------------------------------------- //
 
 })
